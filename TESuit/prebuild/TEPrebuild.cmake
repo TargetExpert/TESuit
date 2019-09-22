6c83d9e456382260a56f74b911498b45b8ecf650
Doohoon Kim <invi.dh.kim@gmail.com>
2019-09-23 05:18:09 +0900

    Revised structures of "TEPrebuild.cmake".

--- TESuit/prebuild/TEPrebuild.cmake
+++ TESuit/prebuild/TEPrebuild.cmake
@@ -17,16 +17,36 @@
 #
 #=========================================================================
 
-function(TE_Prebuild)
-	include(${CMAKE_CURRENT_LIST_DIR}/TESuit/prebuild/TEAutoInc.cmake)
+# including TEAutoTarget source for finding Target list.
+include(${_TESuit_Dir_Name}/${_TESuit_prebuild_Dir_Name}/TEAutoTarget.cmake)
+# including TEAutoInc source for including Source & Directory.
+include(${_TESuit_Dir_Name}/${_TESuit_prebuild_Dir_Name}/TEAutoInc.cmake)
 
-	TE_AutoInc(./TargetExpert ".c;.cpp;.h;.hpp;.cc")
+function(_Prebuild_DiscriminateTarget)
+	# Find the source for Target LLL.
+	_Find_Target(${_TESuit_Dir_Name}/${_TESuit_arch_Dir_Name} ".folist")
+endfunction()
 
-	include(${CMAKE_CURRENT_LIST_DIR}/TESuit/arch/avr8.prebuild.cmake)
+function(_Prebuild_IncludingSource)
+	# Find the source for Target LLL.
+	_Find_SrcAndDir(${_TargetExpert_Dir_Name} ".c;.cc;.cpp;.h;.hpp")
+
+	set(TE_src_files ${TE_src_files} PARENT_SCOPE)
+	set(TE_src_directories ${TE_src_directories} PARENT_SCOPE)
+endfunction()
+
+function(Prebuild_TE)
+	_Prebuild_DiscriminateTarget()
+	_Prebuild_IncludingSource()
+
+	include(${_TESuit_Dir_Name}/${_TESuit_arch_Dir_Name}/avr/avr.prebuild.cmake)
 
 	set(TE_src_files ${TE_src_files} PARENT_SCOPE)
 	set(TE_src_directories ${TE_src_directories} PARENT_SCOPE)
 
 	include_directories(./)
 	include_directories(${TE_src_directories})
+
+	# Including for "include directory" on toolchain root.
+	include_directories(${_TESuit_Dir_Name}/${toolchains}/avr/avr/include)
 endfunction()
