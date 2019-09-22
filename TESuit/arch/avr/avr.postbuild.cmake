0669a6cca09d9d2cd27425c1a5585a904490ab68
Doohoon Kim <invi.dh.kim@gmail.com>
2019-09-23 08:06:07 +0900

    Changed the reference parts of the postbuild file for AVR.

--- TESuit/arch/avr/avr.postbuild.cmake
+++ TESuit/arch/avr/avr.postbuild.cmake
@@ -18,21 +18,21 @@
 #=========================================================================
 
 add_custom_command(TARGET ${PROJECT_NAME}
-		POST_BUILD COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/TESuit/toolchains/avr/bin/avr-objcopy
+		POST_BUILD COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/${_TESuit_Dir_Name}/${_TESuit_toolchains_Dir_Name}/${_TESuit_Target_Arch_Name}/bin/avr-objcopy
 		-O ihex -R.eeprom
 		${CMAKE_CURRENT_SOURCE_DIR}/cmake-build-debug/${PROJECT_NAME}
 		${CMAKE_CURRENT_SOURCE_DIR}/cmake-build-debug/${PROJECT_NAME}.hex)
 add_custom_command(TARGET ${PROJECT_NAME}
-		POST_BUILD COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/TESuit/toolchains/avr/bin/avr-objcopy
+		POST_BUILD COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/${_TESuit_Dir_Name}/${_TESuit_toolchains_Dir_Name}/${_TESuit_Target_Arch_Name}/bin/avr-objcopy
 		-O ihex -j .eeprom --set-section-flags=.eeprom=alloc,load --no-change-warnings --change-section-lma .eeprom=0
 		${CMAKE_CURRENT_SOURCE_DIR}/cmake-build-debug/${PROJECT_NAME}
 		${CMAKE_CURRENT_SOURCE_DIR}/cmake-build-debug/${PROJECT_NAME}.eep)
 add_custom_command(TARGET ${PROJECT_NAME}
-		POST_BUILD COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/TESuit/toolchains/avr/bin/avr-size
+		POST_BUILD COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/${_TESuit_Dir_Name}/${_TESuit_toolchains_Dir_Name}/${_TESuit_Target_Arch_Name}/bin/avr-size
 		${CMAKE_CURRENT_SOURCE_DIR}/cmake-build-debug/${PROJECT_NAME}
-		-C --mcu=${TARGET} --format=avr)
+		-C --mcu=${_TESuit_Target} --format=${_TESuit_Target_Arch_Name})
 add_custom_command(TARGET ${PROJECT_NAME}
-		POST_BUILD COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/TESuit/toolchains/avr/bin/avrdude
-		-C${CMAKE_CURRENT_SOURCE_DIR}/TESuit/toolchains/avr/etc/avrdude.conf
-		-v -p${TARGET} -c${DEBUGGER_TYPE} -P${DEBUGGER_PORT} -b115200
+		POST_BUILD COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/${_TESuit_Dir_Name}/${_TESuit_toolchains_Dir_Name}/${_TESuit_Target_Arch_Name}/bin/avrdude
+		-C${CMAKE_CURRENT_SOURCE_DIR}/${_TESuit_Dir_Name}/${_TESuit_toolchains_Dir_Name}/${_TESuit_Target_Arch_Name}/etc/avrdude.conf
+		-v -p${_TESuit_Target} -c${_TESuit_Joint_Interface} -P${_TESuit_Joint_Port} -b115200
 		-D -Uflash:w:${PROJECT_NAME}.hex:i)
