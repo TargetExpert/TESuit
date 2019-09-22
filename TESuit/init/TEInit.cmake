89dc47f4a95760ab7245d8219ec58a2f2378bd90
Doohoon Kim <invi.dh.kim@gmail.com>
2019-09-23 07:51:15 +0900

    Added functions for Target machine settings.

--- TESuit/init/TEInit.cmake
+++ TESuit/init/TEInit.cmake
@@ -18,11 +18,8 @@
 
 # including TELangOpt source for setting Language options.
 include(${_TESuit_Dir_Name}/${_TESuit_init_Dir_Name}/TELangOpt.cmake)
-
-# Internal functions.
-function(_Init_Machines)
-
-endfunction()
+# including TEAutoTarget source for finding Target list.
+include(${_TESuit_Dir_Name}/${_TESuit_init_Dir_Name}/TEAutoTarget.cmake)
 
 # External functions.
 function(Set_Language_TE lang_type)
@@ -53,6 +50,30 @@ function(Set_LangOptAll_TE lang_std lang_type lang_year)
 	set(_TESuit_LangStdYear ${_TESuit_LangStdYear} PARENT_SCOPE)
 endfunction()
 
+function(Set_Target target)
+	set(__Is_Set_Target true PARENT_SCOPE)
+
+	set(_TESuit_Target ${target} PARENT_SCOPE)
+endfunction()
+
+function(Set_TargetFreq frequency)
+	set(__Is_Set_Target true PARENT_SCOPE)
+
+	set(_TESuit_Target_Freq ${frequency} PARENT_SCOPE)
+endfunction()
+
+function(Set_JointInterface interface_name)
+	set(__Is_Set_Target true PARENT_SCOPE)
+
+	set(_TESuit_Joint_Interface ${interface_name} PARENT_SCOPE)
+endfunction()
+
+function(Set_JointPort port)
+	set(__Is_Set_Target true PARENT_SCOPE)
+
+	set(_TESuit_Joint_Port ${port} PARENT_SCOPE)
+endfunction()
+
 # The "TE_Init" function is called after all other "External functions" have been called.
 function(Init_TE)
 	if (__Is_Set_LangOpt)
@@ -61,7 +82,12 @@ function(Init_TE)
 		_Set_ForceLangOpt()
 	endif()
 
-	_Init_Machines()
+	if(__Is_Set_Target)
+		# Find the source for Target LLL.
+		_Find_Target(${_TESuit_Dir_Name}/${_TESuit_arch_Dir_Name} ".folist")
+#	else()
+#		message(FATAL_ERROR "Please, Set the your \"Target Device\" and \"Target Interface\".")
+	endif()
 
 	if(_TESuit_LangType STREQUAL "c")
 		set(CMAKE_C_STANDARD ${_TESuit_LangStdYear} PARENT_SCOPE)
@@ -74,4 +100,6 @@ function(Init_TE)
 	set(_TESuit_LangStdType ${_TESuit_LangStdType} PARENT_SCOPE)
 	set(_TESuit_LangStdTypeParm ${_TESuit_LangStdTypeParm} PARENT_SCOPE)
 	set(_TESuit_LangStdYear ${_TESuit_LangStdYear} PARENT_SCOPE)
+
+	set(_TESuit_Target_Arch_Name ${_TESuit_Target_Arch_Name} PARENT_SCOPE)
 endfunction()
