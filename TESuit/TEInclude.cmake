4e954c16a9382380ba32d57dd747506b7d5d9b3d
Doohoon Kim <invi.dh.kim@gmail.com>
2019-09-23 05:05:24 +0900

    The path to include multiple scripts in "TEInclude.cmake" has been changed to be operated by a variable.

--- TESuit/TEInclude.cmake
+++ TESuit/TEInclude.cmake
@@ -1,6 +1,5 @@
 #=========================================================================
 #
-#          File: TEAutoInc.cmake
 #          File: TEInclude.cmake
 #
 #   Description: Include scripts for Prebuild and Postbuild.
@@ -17,5 +16,8 @@
 #
 #=========================================================================
 
-include(${CMAKE_CURRENT_LIST_DIR}/prebuild/TEPrebuild.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/postbuild/TEPostbulid.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/TEVars.cmake)
+
+include(${_TESuit_Dir_Name}/${_TESuit_init_Dir_Name}/TEInit.cmake)
+include(${_TESuit_Dir_Name}/${_TESuit_prebuild_Dir_Name}/TEPrebuild.cmake)
+include(${_TESuit_Dir_Name}/${_TESuit_postbuild_Dir_Name}/TEPostbuild.cmake)
