2fe4a1d1339164627b1821d2ff46b07be93317e7
Doohoon Kim <invi.dh.kim@gmail.com>
2019-09-23 08:16:15 +0900

    Deleted unnecessary settings and changed parts that were set to absolute paths.

--- TESuit/prebuild/TEPrebuild.cmake
+++ TESuit/prebuild/TEPrebuild.cmake
@@ -17,36 +17,18 @@
 #
 #=========================================================================
 
-# including TEAutoTarget source for finding Target list.
-include(${_TESuit_Dir_Name}/${_TESuit_prebuild_Dir_Name}/TEAutoTarget.cmake)
 # including TEAutoInc source for including Source & Directory.
 include(${_TESuit_Dir_Name}/${_TESuit_prebuild_Dir_Name}/TEAutoInc.cmake)
 
-function(_Prebuild_DiscriminateTarget)
-	# Find the source for Target LLL.
-	_Find_Target(${_TESuit_Dir_Name}/${_TESuit_arch_Dir_Name} ".folist")
-endfunction()
-
-function(_Prebuild_IncludingSource)
+function(Prebuild_TE)
 	# Find the source for Target LLL.
 	_Find_SrcAndDir(${_TargetExpert_Dir_Name} ".c;.cc;.cpp;.h;.hpp")
 
-	set(TE_src_files ${TE_src_files} PARENT_SCOPE)
-	set(TE_src_directories ${TE_src_directories} PARENT_SCOPE)
-endfunction()
-
-function(Prebuild_TE)
-	_Prebuild_DiscriminateTarget()
-	_Prebuild_IncludingSource()
-
-	include(${_TESuit_Dir_Name}/${_TESuit_arch_Dir_Name}/avr/avr.prebuild.cmake)
+	include(${_TESuit_Dir_Name}/${_TESuit_arch_Dir_Name}/${_TESuit_Target_Arch_Name}/${_TESuit_Target_Arch_Name}.prebuild.cmake)
 
 	set(TE_src_files ${TE_src_files} PARENT_SCOPE)
 	set(TE_src_directories ${TE_src_directories} PARENT_SCOPE)
 
 	include_directories(./)
 	include_directories(${TE_src_directories})
-
-	# Including for "include directory" on toolchain root.
-	include_directories(${_TESuit_Dir_Name}/${toolchains}/avr/avr/include)
 endfunction()
